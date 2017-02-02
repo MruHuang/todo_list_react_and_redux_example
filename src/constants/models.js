2b4809c43e3c363ff62250b7e003b39029a330e9
Hsin-Hao Tang <cythilya@gmail.com>
2017-02-02 18:10:20 +0800

    remove useless fields

--- src/constants/models.js
+++ src/constants/models.js
@@ -3,11 +3,8 @@ import Immutable from 'immutable';
 export const TodoState = Immutable.fromJS({
   'todos': [],
   'todo': {
-    id: '',
     text: '',
-    updatedAt: '',
     completed: false,
   },
   filter: 'show_all'
 });
-
